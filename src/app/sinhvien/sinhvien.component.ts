74acc61de3ad2adebdcdd8f934f9c1dc59dfb03b
Sinner81120 <54507793+Sinner81120@users.noreply.github.com>
2019-09-15 02:56:33 +0700

    lab02

--- src/app/sinhvien/sinhvien.component.ts
+++ src/app/sinhvien/sinhvien.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SinhvienComponent implements OnInit {
   find;
+  page : any;
   itemsPerPage = 3;
   p: number = 1;
   students = [
