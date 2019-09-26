c054cde6845c6d8ca446797c7040699d2917edce
Sinner81120 <54507793+Sinner81120@users.noreply.github.com>
2019-09-26 14:37:14 +0700

    fix

--- src/app/Lab03/giaodien/giaodien.component.ts
+++ src/app/Lab03/giaodien/giaodien.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./giaodien.component.css']
 })
 export class GiaodienComponent implements OnInit {
-
+  titles = [
+    {
+      title: "Máy tính xách tay",
+      loai: "Laptop"
+    },
+    {
+      title: "Điện thoại di động",
+      loai: "Phone"
+    },
+    {
+      title: "Quạt máy",
+      loai: "Fan"
+    },
+    {
+      title: "Nhẫn",
+      loai: "Ring"
+    },
+    {
+      title: "Xe máy",
+      loai: "Car"
+    },
+    {
+      title: "Đồng hồ",
+      loai: "Watch"
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
